48247dc82305483841a214e2532e798d1e02ddf9
Parv7-J <parvjain19012006@gmail.com>
2025-07-22 19:29:01 +0530

    three-way-handshake complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use std::{fmt, io};
 
+const SYN_FLAG: u8 = 0b0000_0010;
+const ACK_FLAG: u8 = 0b0001_0000;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 #[repr(u8)]
 pub enum Protocol {
@@ -64,6 +67,202 @@ pub struct Packet {
     pub options: Option<Options>,
 }
 
+#[derive(Debug)]
+pub struct Segment {
+    pub source_port: u16,
+    pub destination_port: u16,
+    pub sequence_number: u32,
+    pub acknowledgement_number: u32,
+    pub data_offset: u8,
+    pub flags: u8,
+    pub window: u16,
+    pub checksum: u16,
+    pub urgent_pointer: u16,
+    pub options: Option<Options>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum State {
+    Listen,
+    SynRcvd,
+    Established,
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+pub struct Connection {
+    pub state: State,
+    pub source_address: u32,
+    pub source_port: u16,
+    pub destination_address: u32,
+    pub destination_port: u16,
+    pub send_next: u32,
+    pub recv_next: u32,
+}
+
+pub enum RequestType {
+    SYN,
+    ACK,
+    Unknown(u8),
+}
+
+impl Segment {
+    pub fn parse(segment_data: &[u8], pseudo_header: [u16; 6]) -> Result<Self, io::Error> {
+        if segment_data.len() < 20 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Segment too short for TCP header",
+            ));
+        }
+
+        if !Self::validate(&segment_data, pseudo_header) {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Segment Checksum Invalid",
+            ));
+        }
+
+        let source_port = u16::from_be_bytes([segment_data[0], segment_data[1]]);
+
+        let destination_port = u16::from_be_bytes([segment_data[2], segment_data[3]]);
+
+        let sequence_number = u32::from_be_bytes([
+            segment_data[4],
+            segment_data[5],
+            segment_data[6],
+            segment_data[7],
+        ]);
+
+        let acknowledgement_number = u32::from_be_bytes([
+            segment_data[8],
+            segment_data[9],
+            segment_data[10],
+            segment_data[11],
+        ]);
+
+        let data_offset = segment_data[12] >> 4;
+
+        let flags = segment_data[13];
+
+        let window = u16::from_be_bytes([segment_data[14], segment_data[15]]);
+
+        let checksum = u16::from_be_bytes([segment_data[16], segment_data[17]]);
+
+        let urgent_pointer = u16::from_be_bytes([segment_data[18], segment_data[19]]);
+
+        let options: Option<Options> = None;
+
+        Ok(Segment {
+            source_port,
+            destination_port,
+            sequence_number,
+            acknowledgement_number,
+            data_offset,
+            flags,
+            window,
+            checksum,
+            urgent_pointer,
+            options,
+        })
+    }
+
+    fn validate(segment_data: &[u8], pseudo_header: [u16; 6]) -> bool {
+        let mut sum: u32 = 0;
+        for i in pseudo_header {
+            sum += i as u32;
+        }
+        for bytes in segment_data.chunks_exact(2) {
+            sum += u16::from_be_bytes([bytes[0], bytes[1]]) as u32;
+        }
+        let rem = segment_data.chunks_exact(2).remainder();
+
+        if rem.len() == 1 {
+            sum += u16::from_be_bytes([rem[0], 0]) as u32;
+        }
+
+        while (sum >> 16) > 0 {
+            sum = (sum >> 16) + (sum & 0xFFFF);
+        }
+
+        return sum == 0xFFFF;
+    }
+
+    pub fn check_flags(flags: u8) -> RequestType {
+        //we right now only handle if syn and if ack
+        if ((flags >> 4) & 1 == 0) && ((flags >> 1) & 1 == 1) {
+            return RequestType::SYN;
+        } else if ((flags >> 4) & 1 == 1) && ((flags >> 1) & 1 == 0) {
+            return RequestType::ACK;
+        } else {
+            return RequestType::Unknown(flags);
+        }
+    }
+
+    pub fn build(connection: &Connection) -> Result<[u8; 20], io::Error> {
+        let mut tcp_header = [0u8; 20];
+
+        tcp_header[0..2].copy_from_slice(&connection.destination_port.to_be_bytes());
+
+        tcp_header[2..4].copy_from_slice(&connection.source_port.to_be_bytes());
+
+        let seq = (connection.send_next - 1).to_be_bytes();
+        tcp_header[4..8].copy_from_slice(&seq);
+
+        let ack = connection.recv_next.to_be_bytes();
+        tcp_header[8..12].copy_from_slice(&ack);
+
+        tcp_header[12] = 5 << 4;
+
+        tcp_header[13] = SYN_FLAG | ACK_FLAG;
+
+        let window = (65535 as u16).to_be_bytes();
+        tcp_header[14..16].copy_from_slice(&window);
+
+        let pseudo_header = [
+            (connection.destination_address >> 16) as u16,
+            (connection.destination_address & 0xFFFF) as u16,
+            (connection.source_address >> 16) as u16,
+            (connection.source_address & 0xFFFF) as u16,
+            6,
+            20,
+        ];
+
+        let checksum = Self::calculate_checksum(&tcp_header, pseudo_header).to_be_bytes();
+
+        tcp_header[16..18].copy_from_slice(&checksum);
+
+        if !Self::validate(&tcp_header, pseudo_header) {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Checksum calculation wrong",
+            ));
+        }
+
+        return Ok(tcp_header);
+    }
+
+    fn calculate_checksum(tcp_header: &[u8], pseudo_header: [u16; 6]) -> u16 {
+        let mut sum: u32 = 0;
+        for byte in pseudo_header {
+            sum += byte as u32;
+        }
+        for byte in tcp_header.chunks_exact(2) {
+            sum += u16::from_be_bytes([byte[0], byte[1]]) as u32;
+        }
+
+        while (sum >> 16) > 0 {
+            sum = (sum >> 16) + (sum & 0xFFFF);
+        }
+
+        return !sum as u16;
+    }
+}
+
 impl Packet {
     pub fn parse(packet_data: &[u8]) -> Result<Self, io::Error> {
         if packet_data.len() < 20 {
@@ -73,34 +272,35 @@ impl Packet {
             ));
         }
 
-        // Byte 0: Version (4 bits) and IHL (4 bits)
-        let version = packet_data[0] >> 4;
         let ihl = packet_data[0] & 0x0F;
 
-        // Byte 1: DSCP (6 bits) and ECN (2 bits)
+        let header_length = ihl * 4;
+
+        if !Self::validate(&packet_data[..header_length as usize]) {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Packet Checksum Invalid",
+            ));
+        };
+
+        let version = packet_data[0] >> 4;
+
         let dscp = packet_data[1] >> 2;
         let ecn = packet_data[1] & 0x03;
 
-        // Bytes 2-3: Total Length
         let total_length = u16::from_be_bytes([packet_data[2], packet_data[3]]);
 
-        // Bytes 4-5: Identification
         let identification = u16::from_be_bytes([packet_data[4], packet_data[5]]);
 
-        // Bytes 6-7: Flags (3 bits) and Fragment Offset (13 bits)
         let flags = packet_data[6] >> 5;
         let fragment_offset = u16::from_be_bytes([packet_data[6] & 0x1F, packet_data[7]]);
 
-        // Byte 8: Time To Live (TTL)
         let ttl = packet_data[8];
 
-        // Byte 9: Protocol
         let protocol = Protocol::from_u8(packet_data[9]);
 
-        // Bytes 10-11: Header Checksum
         let header_checksum = u16::from_be_bytes([packet_data[10], packet_data[11]]);
 
-        // Bytes 12-15: Source Address
         let source_address = u32::from_be_bytes([
             packet_data[12],
             packet_data[13],
@@ -108,7 +308,6 @@ impl Packet {
             packet_data[15],
         ]);
 
-        // Bytes 16-19: Destination Address
         let destination_address = u32::from_be_bytes([
             packet_data[16],
             packet_data[17],
@@ -134,5 +333,82 @@ impl Packet {
             destination_address,
             options,
         })
+    }
+
+    fn validate(packet_data: &[u8]) -> bool {
+        // let mut sum: u32 = 0;
+        // for i in packet_data.chunks_exact(2) {
+        //     sum += u16::from_be_bytes([i[0], i[1]]) as u32;
+        // }
+
+        let header_length = (packet_data[0] & 0x0F) * 4;
+        let ip_header = &packet_data[..header_length as usize];
+
+        let mut sum: u32 = 0;
+        for chunk in ip_header.chunks_exact(2) {
+            sum += u16::from_be_bytes([chunk[0], chunk[1]]) as u32;
+        }
+        let rem = packet_data.chunks_exact(2).remainder();
+
+        if rem.len() == 1 {
+            sum += u16::from_be_bytes([rem[0], 0]) as u32;
+        }
+
+        while (sum >> 16) > 0 {
+            sum = (sum & 0xFFFF) + (sum >> 16);
+        }
+
+        return sum == 0xFFFF;
+    }
+
+    pub fn build(tcp_header: [u8; 20], connection: &Connection) -> Result<[u8; 40], io::Error> {
+        let mut ip_header = [0u8; 20];
+
+        ip_header[0] = (4 << 4) | 5;
+
+        let length = (40 as u16).to_be_bytes();
+
+        ip_header[2..4].copy_from_slice(&length);
+
+        ip_header[6] = 2 << 5;
+
+        ip_header[8] = 64;
+
+        ip_header[9] = 6;
+
+        ip_header[12..16].copy_from_slice(&connection.destination_address.to_be_bytes());
+
+        ip_header[16..20].copy_from_slice(&connection.source_address.to_be_bytes());
+
+        let checksum = Self::calculate_checksum(&ip_header).to_be_bytes();
+
+        ip_header[10..12].copy_from_slice(&checksum);
+
+        if !Self::validate(&ip_header) {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Checksum calculation wrong",
+            ));
+        }
+
+        let mut ip_packet = [0u8; 40];
+        ip_packet[0..20].copy_from_slice(&ip_header);
+        ip_packet[20..40].copy_from_slice(&tcp_header);
+
+        return Ok(ip_packet);
+    }
+
+    fn calculate_checksum(ip_packet: &[u8]) -> u16 {
+        let mut sum: u32 = 0;
+
+        for byte in ip_packet.chunks_exact(2) {
+            sum += u16::from_be_bytes([byte[0], byte[1]]) as u32;
+        }
+
+        while (sum >> 16) > 0 {
+            sum = (sum >> 16) + (sum & 0xFFFF);
+        }
+
+        return !sum as u16;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,33 @@
 use std::{
+    collections::HashMap,
     fs::File,
-    io::{self, Read},
+    io::{self, Read, Write},
     os::fd::AsRawFd,
-    process::{self, Command, Stdio},
+    process::{Command, Stdio},
+    sync::atomic::{AtomicBool, Ordering},
 };
-use tcp_scratch::{Packet, Protocol};
 
-struct IptablesGuard {
-    device_name: String,
-}
+use tcp_scratch::{Connection, Packet, Protocol, RequestType, Segment, State};
 
-impl IptablesGuard {
-    fn new(device_name: &str) -> io::Result<Self> {
-        println!(
-            "Adding iptables rule to prevent kernel RST packets on {}...",
-            device_name
-        );
-        let status = Command::new("sudo")
-            .args([
-                "iptables",
-                "-I",
-                "INPUT",
-                "1",
-                "-i",
-                device_name,
-                "-p",
-                "tcp",
-                "-j",
-                "DROP",
-            ])
-            .status()?;
+static SHUTDOWN_FLAG: AtomicBool = AtomicBool::new(false);
 
-        if !status.success() {
-            return Err(io::Error::new(
-                io::ErrorKind::Other,
-                "Failed to add iptables rule",
-            ));
-        }
-
-        Ok(IptablesGuard {
-            device_name: device_name.to_string(),
-        })
-    }
-}
-
-impl Drop for IptablesGuard {
-    fn drop(&mut self) {
-        println!("\nCleaning up iptables rule for {}...", self.device_name);
-        let status = Command::new("sudo")
-            .args([
-                "iptables",
-                "-D",
-                "INPUT",
-                "-i",
-                &self.device_name,
-                "-p",
-                "tcp",
-                "-j",
-                "DROP",
-            ])
-            .status();
-
-        match status {
-            Ok(s) if s.success() => println!("Successfully removed iptables rule."),
-            _ => eprintln!("Error: Failed to remove iptables rule. Please remove it manually."),
-        }
-    }
+extern "C" fn handle_sigint(_signal: i32) {
+    SHUTDOWN_FLAG.store(true, Ordering::Relaxed);
 }
 
 fn main() -> io::Result<()> {
-    let if_name = "tun0";
-    let ip_address = "10.0.0.1/24";
+    let mut connections: HashMap<(u32, u16, u32, u16), Connection> = HashMap::new();
 
-    let if_name_clone = if_name.to_string();
-    ctrlc::set_handler(move || {
-        println!("\nCtrl+C received. Cleaning up and exiting.");
-
-        // Run the cleanup command to remove the iptables rule.
-        let status = Command::new("sudo")
-            .args([
-                "iptables",
-                "-D",
-                "INPUT",
-                "-i",
-                &if_name_clone,
-                "-p",
-                "tcp",
-                "-j",
-                "DROP",
-            ])
-            .status()
-            .expect("Failed to run iptables cleanup command.");
-
-        if status.success() {
-            println!("Successfully removed iptables rule.");
-        } else {
-            eprintln!("Error: Failed to remove iptables rule. Please remove it manually.");
+    unsafe {
+        let mut action: libc::sigaction = std::mem::zeroed();
+        action.sa_sigaction = handle_sigint as libc::size_t;
+        if libc::sigaction(libc::SIGINT, &action, std::ptr::null_mut()) != 0 {
+            panic!("Failed to register signal handler.");
         }
+    }
 
-        // Exit the process cleanly.
-        process::exit(0);
-    })
-    .expect("Error setting Ctrl-C handler");
+    let tunif_name = "tun0";
+    let tunif_address = "10.0.0.1/24";
 
     let mut tun_file = File::options()
         .read(true)
@@ -110,61 +36,200 @@ fn main() -> io::Result<()> {
 
     let tun_fd = tun_file.as_raw_fd();
 
-    let mut req: libc::ifreq = unsafe { std::mem::zeroed() };
+    let mut tunif_req: libc::ifreq = unsafe { std::mem::zeroed() };
 
-    for (i, ch) in if_name.as_bytes().iter().enumerate() {
-        req.ifr_name[i] = *ch as libc::c_char;
+    for (i, byte) in tunif_name.as_bytes().iter().enumerate() {
+        tunif_req.ifr_name[i] = *byte as libc::c_char;
     }
 
-    req.ifr_ifru.ifru_flags = libc::IFF_TUN as i16 | libc::IFF_NO_PI as i16;
+    tunif_req.ifr_ifru.ifru_flags = libc::IFF_NO_PI as i16 | libc::IFF_TUN as i16;
 
-    let res = unsafe { libc::ioctl(tun_fd, libc::TUNSETIFF as u64, &req) };
+    let tunif_res = unsafe { libc::ioctl(tun_fd, libc::TUNSETIFF as u64, &tunif_req) };
 
-    if res < 0 {
+    if tunif_res < 0 {
         return Err(io::Error::last_os_error());
     }
 
-    println!("TUN interface set succesfully, now configuration begins");
+    println!("TUN is established!");
+
+    Command::new("sudo")
+        .args([
+            "iptables",
+            "-I",
+            "INPUT",
+            "1",
+            "-i",
+            tunif_name,
+            "-p",
+            "tcp",
+            "--tcp-flags", // This is the new part
+            "SYN,RST,ACK,FIN",
+            "SYN", // This is the new part
+            "-j",
+            "DROP",
+        ])
+        .status()?;
 
     Command::new("sudo")
         .args([
             "sysctl",
             "-w",
-            &format!("net.ipv6.conf.{}.disable_ipv6=1", if_name),
+            &format!("net.ipv6.conf.{}.disable_ipv6=1", tunif_name),
         ])
         .stdout(Stdio::null())
         .status()?;
 
     Command::new("sudo")
-        .args(["ip", "link", "set", "dev", if_name, "up"])
+        .args(["ip", "link", "set", "dev", tunif_name, "up"])
         .status()?;
 
     Command::new("sudo")
-        .args(["ip", "addr", "add", ip_address, "dev", if_name])
+        .args(["ip", "addr", "add", tunif_address, "dev", { tunif_name }])
         .status()?;
 
-    let _guard = IptablesGuard::new(if_name)?;
-
-    println!("Interface {} configured successfully.", if_name);
+    println!("TUN is configured!");
 
     let mut buff = [0u8; 2000];
 
     loop {
-        let n = tun_file.read(&mut buff)?;
-
-        if n == 0 {
-            println!("TUN device closed, exiting.");
+        if SHUTDOWN_FLAG.load(Ordering::Relaxed) {
             break;
         }
 
+        let n = match tun_file.read(&mut buff) {
+            Ok(0) => {
+                println!("EOF reached!");
+                break;
+            }
+            Ok(n) => n,
+            Err(e) if e.kind() == io::ErrorKind::Interrupted => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        };
+
         let packet = Packet::parse(&buff[..n])?;
 
-        if let Protocol::TCP = packet.protocol {
-            println!("{packet:?}");
+        if packet.protocol == Protocol::TCP {
+            println!("Hey tcp is here!!!");
+
+            let length_of_ip_header = packet.ihl * 4;
+            let length_of_segment = packet.total_length - length_of_ip_header as u16;
+
+            let pseudo_header = [
+                (packet.source_address >> 16) as u16,
+                (packet.source_address & 0xFFFF) as u16,
+                (packet.destination_address >> 16) as u16,
+                (packet.destination_address & 0xFFFF) as u16,
+                6,
+                length_of_segment,
+            ];
+
+            let segment = Segment::parse(&buff[length_of_ip_header as usize..n], pseudo_header)?;
+
+            // let segment_header_length = segment.data_offset * 4;
+
+            // let data_size = length_of_segment - segment_header_length as u16;
+
+            let segment_type = Segment::check_flags(segment.flags);
+
+            match segment_type {
+                RequestType::SYN => {
+                    if let Some(pckt) = hanlde_syn(&packet, &segment, &mut connections) {
+                        tun_file.write_all(&pckt)?
+                    };
+                }
+                RequestType::ACK => {
+                    hanlde_ack(&packet, &segment);
+                }
+                RequestType::Unknown(_) => {
+                    continue;
+                }
+            }
         } else {
-            println!("Packet is not tcp, it is {}", packet.version);
+            println!("Packet is {}", packet.protocol);
         }
     }
 
+    Command::new("sudo")
+        .args([
+            "iptables",
+            "-D", // Use -D for Delete
+            "INPUT",
+            "-i",
+            tunif_name,
+            "-p",
+            "tcp",
+            "--tcp-flags",
+            "SYN,RST,ACK,FIN",
+            "SYN",
+            "-j",
+            "DROP",
+        ])
+        .status()?;
+
+    println!("TUN is cleaned up!");
+
     Ok(())
+}
+
+fn hanlde_syn(
+    packet: &Packet,
+    segment: &Segment,
+    connections: &mut HashMap<(u32, u16, u32, u16), Connection>,
+) -> Option<[u8; 40]> {
+    let isn = 0;
+
+    let connection = Connection {
+        state: State::SynRcvd,
+        source_address: packet.source_address,
+        source_port: segment.source_port,
+        destination_address: packet.destination_address,
+        destination_port: segment.destination_port,
+        send_next: isn + 1,
+        recv_next: segment.sequence_number + 1,
+    };
+
+    if connections.contains_key(&(
+        connection.source_address,
+        connection.source_port,
+        connection.destination_address,
+        connection.destination_port,
+    )) {
+        return None;
+    }
+
+    let tcp_header = match Segment::build(&connection) {
+        Ok(header) => header,
+        Err(_) => {
+            println!("Checksum calc is not done correctly for segment");
+            return None;
+        }
+    };
+
+    let ip_packet = match Packet::build(tcp_header, &connection) {
+        Ok(packet) => packet,
+        Err(_) => {
+            println!("Checksum calc is not done correctly for packet");
+            return None;
+        }
+    };
+
+    connections.insert(
+        (
+            connection.source_address,
+            connection.source_port,
+            connection.destination_address,
+            connection.destination_port,
+        ),
+        connection,
+    );
+
+    return Some(ip_packet);
+}
+
+fn hanlde_ack(packet: &Packet, segment: &Segment) {
+    println!("ACK received");
 }
